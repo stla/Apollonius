0ce86b2d5fa939ce5ba3745c723e701f40128ed8
stla <laurent_step@outlook.fr>
2023-07-06 00:44:47 +0200

    fix

--- inst/essais/essai03.R
+++ inst/essais/essai03.R
@@ -28,14 +28,23 @@ stuff <- Apollonius:::test(sites, radii)
 vertices <- stuff[["vertices"]]
 
 neighbors <- stuff[["neighbors"]]
-Neighs <- c()
-x <- na.omit(neighbors[1L, ])[1L]
-Neighs[1L] <- which(neighbors[1L, ] == x[1L])
+x <- na.omit(neighbors[1L, ])
+Neighs <- vector("list", nrow(neighbors))
+Neighs[[1L]] <- which(neighbors[1L, ] %in% x) -> y
 for(i in 2L:nrow(neighbors)) {
-  x <-
-    setdiff(na.omit(neighbors[i, ]), neighbors[cbind(1L:(i-1L), Neighs[1L:(i-1L)])])
-  Neighs[i] <- which(neighbors[i, ] == x[1L])
+  x <- na.omit(neighbors[i, ])
+  Neighs[[i]] <- 1:3#which(neighbors[i, ] %in% x)
+  remove <- which(is.na(neighbors[i, ]))
+  for(j in 1L:(i-1L)) {
+    if(j %in% neighbors[i, ] && i %in% neighbors[j, ]) {
+      remove <- c(remove, which(neighbors[i, ] == j))
+    }
+  }
+  if(length(remove) > 0L) {
+    Neighs[[i]] <- Neighs[[i]][-remove]
+  }
 }
+Neighs
 
 commonVertices <-
   abind::abind(stuff[["cvertex1"]], stuff[["cvertex2"]], along = 3L)
@@ -46,50 +55,29 @@ dpoints <- stuff[["dpoints"]]
 points(dpoints, pch = 19)
 
 
-# check equidistances:
-P1 <- dpoints[1L, ]
-vs <- commonVertices[1L, Neighs[1L], ]
-A <- vertices[[1L]][vs[1L], 1L:2L]
-rA <- vertices[[1L]][vs[1L], 3L]
-B <- vertices[[1L]][vs[2L], 1L:2L]
-rB <- vertices[[1L]][vs[2L], 3L]
-sqrt(c(crossprod(P1 - A))) - rA
-sqrt(c(crossprod(P1 - B))) - rB
-
-ctr <- (A + B)/2
-P2 <- dpoints[neighbors[1L, Neighs[1L]], ]
-f <- function(s) {
-  d <- ctr + gyromidpoint(P1-ctr, P2-ctr, s)
-  Ad <- d - A
-  Bd <- d - B
-  sqrt(c(crossprod(Ad))) - sqrt(c(crossprod(Bd))) - (rA - rB)
-}
-ur <- uniroot(f, lower = 0.01, upper = 5)
-s <- ur$root
-#points(rbind(ctr), col = "blue", pch = 19)
-hseg <- t(ctr + t(gyrosegment(P1-ctr, P2-ctr, s = s)))
-lines(hseg, col="black", lwd = 2)
 
 
-for(i in 2L:nrow(dpoints)) {
+for(i in 1L:nrow(dpoints)) {
   P1 <- dpoints[i, ]
-  vs <- commonVertices[i, Neighs[i], ]
-  A <- vertices[[i]][vs[1L], 1L:2L]
-  rA <- vertices[[i]][vs[1L], 3L]
-  B <- vertices[[i]][vs[2L], 1L:2L]
-  rB <- vertices[[i]][vs[2L], 3L]
-  ctr <- (A + B)/2
-  P2 <- dpoints[neighbors[i, Neighs[i]], ]
-  f <- function(s) {
-    d <- ctr + gyromidpoint(P1-ctr, P2-ctr, s)
-    Ad <- d - A
-    Bd <- d - B
-    sqrt(c(crossprod(Ad))) - sqrt(c(crossprod(Bd))) - (rA - rB)
+  for(k in seq_along(Neighs[[i]])) {
+    vs <- commonVertices[i, Neighs[[i]][k], ]
+    A <- vertices[[i]][vs[1L], 1L:2L]
+    rA <- vertices[[i]][vs[1L], 3L]
+    B <- vertices[[i]][vs[2L], 1L:2L]
+    rB <- vertices[[i]][vs[2L], 3L]
+    ctr <- (A + B)/2
+    P2 <- dpoints[neighbors[i, Neighs[[i]][k]], ]
+    f <- function(s) {
+      d <- ctr + gyromidpoint(P1-ctr, P2-ctr, s)
+      Ad <- d - A
+      Bd <- d - B
+      sqrt(c(crossprod(Ad))) - sqrt(c(crossprod(Bd))) - (rA - rB)
+    }
+    ur <- uniroot(f, lower = 0.01, upper = 5)
+    s <- ur$root
+    hseg <- t(ctr + t(gyrosegment(P1-ctr, P2-ctr, s = s)))
+    lines(hseg, col="black", lwd = 2)
   }
-  ur <- uniroot(f, lower = 0.01, upper = 5)
-  s <- ur$root
-  hseg <- t(ctr + t(gyrosegment(P1-ctr, P2-ctr, s = s)))
-  lines(hseg, col="black", lwd = 2)
 }
 
 i <- 6L
