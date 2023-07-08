3fa61bb545d6913ca838eaee7b19237bda98431a
stla <laurent_step@yahoo.fr>
2023-07-08 05:32:53 +0200

    sun flower

--- inst/essais/sunflower.R
+++ inst/essais/sunflower.R
@@ -1,7 +1,7 @@
 library(Apollonius)
 
 phi <- pi*(3 - sqrt(5))
-n <- c(0, 50:150)
+n <- c(0, 50:100)
 theta <- n * phi
 r <- sqrt(n) * theta
 x <- r * cos(theta)
@@ -14,3 +14,32 @@ apo <- Apollonius(sites, radii, tmax = 80)
 opar <- par(mar = c(2, 2, 1, 1))
 plotApolloniusGraph(apo, circles = FALSE, color = "random")
 par(opar)
+
+
+fplot <- function(rmax) {
+  radii <- seq(1, rmax, length.out = length(n))
+  apo <- Apollonius(sites, radii, tmax = 80)
+  opar <- par(mar = c(2, 2, 3, 1))
+  plotApolloniusGraph(apo, circles = FALSE, color = "darkred")
+  par(opar)
+}
+
+
+rmax_ <- seq(1, 2, length.out = 20)
+for(i in seq_along(rmax_)) {
+  svg("x.svg", width = 8, height = 8)
+  fplot(rmax_[i])
+  dev.off()
+  rsvg::rsvg_png("x.svg", sprintf("zzpic%03d.png", i), width = 512, height = 512)
+}
+
+library(gifski)
+pngs <- Sys.glob("zzpic*.png")
+gifski(
+  png_files = c(pngs, rev(pngs)),
+  gif_file = "sunflower.gif",
+  width = 512,
+  height = 512,
+  delay = 1/8
+)
+file.remove(pngs)
